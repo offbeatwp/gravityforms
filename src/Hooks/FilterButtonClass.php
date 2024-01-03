c5e6a2dd43f8132756d0eb33b40ea93dc9914406
Maikel Haarmans <maikel@raow.nl>
2024-01-03 11:10:46 +0100

    Update FilterButtonClass.php

--- src/Hooks/FilterButtonClass.php
+++ src/Hooks/FilterButtonClass.php
@@ -5,15 +5,16 @@ final class FilterButtonClass
 {
     public static function buttonClass(array $formSettings, array $form): array
     {
-        $inputVal = esc_attr(rgars($form, 'button/class'));
+        $input = $form['button']['class'] ?? null;
+        $val = is_string($input) ? esc_attr($input) : '';
 
-        $formSettings["Form Button"]["button_class"] = '
+        $formSettings['Form Button']['button_class'] = '
             <tr id="form_button_text_setting" class="child_setting_row">
                 <th>
                     ' . __('Button Class', 'gravityforms') . ' ' . gform_tooltip('form_button_class', '', true) . '
                 </th>
                 <td>
-                    <input type="text" id="form_button_text_class" name="form_button_text_class" class="fieldwidth-3" value="' . $inputVal . '" />
+                    <input type="text" id="form_button_text_class" name="form_button_text_class" class="fieldwidth-3" value="' . $val . '" />
                 </td>
             </tr>';
 
@@ -22,7 +23,9 @@ final class FilterButtonClass
 
     public static function buttonClassProcess(array $updatedForm): array
     {
-        $updatedForm['button']['class'] = rgpost('form_button_text_class');
+        $class = filter_input(INPUT_POST, 'form_button_text_class');
+        $updatedForm['button']['class'] = ($class) ? stripslashes_deep((string)$class) : '';
+
         return $updatedForm;
     }
 }
